3b0d8ccf9c70c0e4dcc461a7b9ee3c210d1efcf2
Logan <ralla0405@gmail.com>
2024-08-30 09:11:57 +0900

    feat: 수강생들 상태 변경 기능 추가

--- src/main/java/org/fastcampus/student_management/domain/Student.java
+++ src/main/java/org/fastcampus/student_management/domain/Student.java
@@ -2,34 +2,48 @@ package org.fastcampus.student_management.domain;
 
 public class Student {
 
-  private final String name;
-  private final int age;
-  private final String address;
-  private boolean activated;
+    private final String name;
+    private final int age;
+    private final String address;
+    private boolean activated;
 
-  public Student(String name, int age, String address) {
-    if (name == null || name.isEmpty()) {
-      throw new IllegalArgumentException("이름은 필수 입력값입니다.");
+    public Student(String name, int age, String address) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("이름은 필수 입력값입니다.");
+        }
+
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.activated = true;
     }
 
-    this.name = name;
-    this.age = age;
-    this.address = address;
-    this.activated = true;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public int getAge() {
+        return age;
+    }
 
-  public int getAge() {
-    return age;
-  }
+    public String getAddress() {
+        return address;
+    }
+    public boolean isActivate() {
+        return activated;
+    }
 
-  public String getAddress() {
-    return address;
-  }
-  public boolean isActivate() {
-    return activated;
-  }
+    public void activate() {
+        if (this.activated) {
+            throw new IllegalArgumentException("이미 활성화된 학생입니다.");
+        }
+        this.activated = true;
+    }
+
+    public void deactivate() {
+        if (!this.activated) {
+            throw new IllegalArgumentException("이미 비활성화된 학생입니다.");
+        }
+        this.activated = false;
+    }
 }
